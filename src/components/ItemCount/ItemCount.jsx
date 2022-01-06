18b0a3aa1f6f640d954b347cbe3bd250e3384f37
Pizi <87288281+Pizi21@users.noreply.github.com>
2022-01-05 22:38:58 -0300

    Ajustes formato

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -10,62 +10,47 @@ const ItemCount = ({stock, initial, onAdd}) => {
 
     const handlerCounterUp =()=>{
         if ( counter < stock ){
-      setCounter(counter + 1);
+            setCounter(counter + 1);
+        }
+    };
+
+    const handlerCounterDown =()=> {
+        if ( counter !==0) {
+          setCounter(counter - 1);
+        }
+    };
+
+    const handlerOnAdd =()=>{
+      onAdd(counter)
+      setCounter(0)
     }
-};
-
-const handlerCounterDown =()=>{
-    if ( counter !==0){
-  setCounter(counter - 1);
-}
-
-};
-
-const handlerOnAdd =()=>{
-  onAdd(counter)
-  setCounter(0)
-}
 
     return (
-
-        <div>
-          
-          
-          <div div className="content">
+      <div>   
+        <div div className="content">
           {/*<div className="header">Producto</div>
           <div className="meta"> Categoria</div>
           <div className="description">Descripcion del producto</div> */}
-    </div>
-          <div div className="extra content"></div>
-           <div className=" ui four buttons">
-             <div className="ui basic red button" onClick={handlerCounterUp}>+</div>
-             <div className="ui basic button"> {counter}</div>
-             <div className=" ui basic green button" onClick={handlerCounterDown}>-</div>
-             {
-               counter >0 ?
-               <div className="ui bottom attached button" onClick={()=> {handlerOnAdd()}
-               }>
-                 <i className="cart icon"></i>
-                 Añadir al carrito
-                 </div>
-                 :
-                 <div className="ui bottom attached button disabled ">
-                   <i className="cart icon"></i>
-                   Añadir al Carrito
-                 </div>
-             }
+        </div>
 
-
-           </div>
-
-          </div>
-          
-
-
-
-         
-        
-        
+        <div div className="extra content"></div>
+        <div className=" ui four buttons">
+          <div className="ui basic red button" onClick={handlerCounterUp}>+</div>
+          <div className="ui basic button"> {counter}</div>
+          <div className=" ui basic green button" onClick={handlerCounterDown}>-</div>
+          {counter > 0 ?
+            <div className="ui bottom attached button" onClick={()=> {handlerOnAdd()}}>
+              <i className="cart icon"></i>
+            Añadir al carrito
+            </div>
+            :
+            <div className="ui bottom attached button disabled ">
+              <i className="cart icon"></i>
+              Añadir al Carrito
+            </div>
+          }
+        </div>
+      </div>
     )
 }
 
